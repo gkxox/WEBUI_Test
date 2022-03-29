7d41104449bddc505d1c9a309316c1bc9a8b555a
gkxox <1135955541@qq.com>
2022-03-29 21:32:08 +0800

    v2

--- run_all.py
+++ run_all.py
@@ -3,9 +3,9 @@ import os
 
 if __name__ == "__main__":
     # 执行pytest单元测试，生成 Allure 报告需要的数据存在 /temp 目录
-    pytest.main(['./Scripts', '--alluredir', './Report/report_data'])
-    # pytest.main()
+    # pytest.main(['./Scripts', '--alluredir', './Report/report_data'])
+    pytest.main()
     # 执行命令 allure generate ./temp -o ./report --clean ，生成测试报告
-    os.system('allure generate ./Report/report_data -o ./Report/report --clean')
-    os.system('powershell.exe  rmdir -r ./Report/report_data/* -Force')
+    # os.system('allure generate ./Report/report_data -o ./Report/report --clean')
+    # os.system('powershell.exe  rmdir -r ./Report/report_data/* -Force')
 
